2fc6837f3c306bea5fbb6088482b55e10aef9838
M1399 <841706879@qq.com>
2016-10-25 02:00:52 +0800

    add init templates

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Article, Comment, User
 
 
 def index(request):
-    latest_question_list = Article.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.article_title for p in latest_question_list])
-    return HttpResponse(output)
-    return HttpResponse("Hello, world. You're at the blog index.")
+    latest_article_list = Article.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_article_list': latest_article_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def login(request):
 	pass
@@ -17,11 +20,22 @@ def register(request):
 	pass
 
 def addArticle(request):
-	pass
+    context = {}
+    template = loader.get_template('blog/addArticle.html')
+    return HttpResponse(template.render(context, request))
 
 def showArticle(request, article_id):
     response = "You're looking at the results of article %s."
     return HttpResponse(response % article_id)
+
+def delArticle(request, article_id):
+    # TODO del 
+    latest_article_list = Article.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_article_list': latest_article_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def addComment(request):
 	pass
