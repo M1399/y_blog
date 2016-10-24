809110f5854b0c3ebdc7f2b49fe8e9a115a84727
M1399 <841706879@qq.com>
2016-10-25 01:06:04 +0800

    add url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Article, Comment, User
+
+
+def index(request):
+    latest_question_list = Article.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.article_title for p in latest_question_list])
+    return HttpResponse(output)
+    return HttpResponse("Hello, world. You're at the blog index.")
+
+def login(request):
+	pass
+
+def register(request):
+	pass
+
+def addArticle(request):
+	pass
+
+def showArticle(request, article_id):
+    response = "You're looking at the results of article %s."
+    return HttpResponse(response % article_id)
+
+def addComment(request):
+	pass
