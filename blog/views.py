05ceabe073cba7a5b633be44f7cdd8ea87722149
M1399 <841706879@qq.com>
2016-10-25 15:39:28 +0800

    init add article

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,13 @@ from django import forms
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
-
+from datetime import datetime
 from .models import Article, Comment, User
 
 import pdb
 
 
+# 首页
 def index(request):
     username = request.session.get('username', 'visitor')
     latest_article_list = Article.objects.order_by('-pub_date')[:5]
@@ -20,21 +21,38 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-def login(request):
-	pass
 
-def register(request):
-	pass
+class ArticleFormForAddArticle(forms.Form):
+    article_title = forms.CharField(max_length=200)
+    article_text = forms.CharField(max_length=200)
 
+
+# 发帖
 def addArticle(request):
-    context = {}
-    template = loader.get_template('blog/addArticle.html')
-    return HttpResponse(template.render(context, request))
+    if not request.session.get('username'):
+        return HttpResponseRedirect('/login') 
+    if request.method == 'POST':
+        form = ArticleFormForAddArticle(request.POST)
+        if form.is_valid():
+            articleSQL = Article()
+            articleSQL.article_title = form.cleaned_data['article_title']
+            articleSQL.article_text = form.cleaned_data['article_text']
+            articleSQL.author = request.session.get('username')
+            articleSQL.pub_date = datetime.now()
+            articleSQL.save()
+            return HttpResponseRedirect('/blog')
+        else:
+            return render(request, 'blog/addArticle.html', {'uf':form})       
+    else:
+        form = ArticleFormForAddArticle()
+    return render(request, 'blog/addArticle.html', {'uf':form})
 
+# 博文详细内容
 def showArticle(request, article_id):
     response = "You're looking at the results of article %s."
     return HttpResponse(response % article_id)
 
+# 删除博文
 def delArticle(request, article_id):
     # TODO del 
     latest_article_list = Article.objects.order_by('-pub_date')[:5]
@@ -46,6 +64,7 @@ def delArticle(request, article_id):
 
 def addComment(request):
 	pass
+
 
 # 登陆相关的表单
 class UserForm(forms.Form):
@@ -86,7 +105,6 @@ def login(request):
         uf = UserForm()
     return render(request, 'blog/login.html', {'uf':uf})
 
-
 # 注册
 def register(request):
     if request.method == 'POST':
@@ -110,7 +128,6 @@ def register(request):
     else:
         uf = UserFormForRegister()
     return render(request,'blog/register.html', {'uf':uf})
-
 
 # 修改密码
 def changePassword(request):
@@ -142,7 +159,6 @@ def changePassword(request):
     else:
         uf = UserFormForChange()
     return render(request,'blog/changePassword.html', {'username':username, 'uf':uf})
-
 
 # 登出
 def logout(request):
