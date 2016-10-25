80113d96c81f58a357c776965e7b791f74231b1d
M1399 <841706879@qq.com>
2016-10-25 14:31:43 +0800

    修改并移除部分注释

--- blog/views.py
+++ blog/views.py
@@ -47,7 +47,7 @@ def delArticle(request, article_id):
 def addComment(request):
 	pass
 
-# 登陆相关
+# 登陆相关的表单
 class UserForm(forms.Form):
     username = forms.CharField(max_length=200)
     password = forms.CharField(max_length=200)
@@ -65,23 +65,19 @@ class UserFormForChange(forms.Form):
     newPasswordAgain = forms.CharField(max_length=200)   
 
 
+# 登陆
 def login(request):
-    # 当提交表单时
     if request.method == 'POST':
-        # form 包含提交的数据
         form = UserForm(request.POST)
-        # 如果提交的数据合法
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']           
-            # 验证
             try:
                 userSQL = User.objects.get(username=username, password=password) 
             except User.DoesNotExist:
                 error = '用户名或密码错误'
                 return render(request, 'blog/login.html', {'error':error}) 
             else:
-                # 传递给session
                 request.session['username'] = username
                 return HttpResponseRedirect('/blog')
         else:
@@ -91,13 +87,10 @@ def login(request):
     return render(request, 'blog/login.html', {'uf':uf})
 
 
-# 注册页面
+# 注册
 def register(request):
-    # 当提交表单时
     if request.method == 'POST':
-        # form 包含提交的数据
         form = UserFormForRegister(request.POST)
-        # 如果提交的数据合法
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
@@ -119,21 +112,17 @@ def register(request):
     return render(request,'blog/register.html', {'uf':uf})
 
 
-# 按登陆之后跳转到的页面
+# 修改密码
 def changePassword(request):
     username = request.session.get('username', 'visitor')
     if(username == 'visitor'):
         return HttpResponse('not logged on')
-    # 当提交表单时
     if request.method == 'POST':
-        # form 包含提交的数据
         form = UserFormForChange(request.POST)
-        # 如果提交的数据合法
         if form.is_valid():
             password = form.cleaned_data['password']
             newPassword = form.cleaned_data['newPassword']
             newPasswordAgain = form.cleaned_data['newPasswordAgain']
-            # 验证
             try:
                 userSQL = User.objects.get(username=username, password=password)
             except User.DoesNotExist:
@@ -155,6 +144,7 @@ def changePassword(request):
     return render(request,'blog/changePassword.html', {'username':username, 'uf':uf})
 
 
+# 登出
 def logout(request):
     if request.session.get('username'):
         del request.session['username']
